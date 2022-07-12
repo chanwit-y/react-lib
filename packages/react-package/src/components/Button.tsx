19f556933fe5df66683d7ef04d906e7ddfb342ff
chanwit-y <chanwit.yimneam@hotmail.com>
2022-07-12 21:56:18 +0700

    +

--- packages/react-package/src/components/Button.tsx
+++ packages/react-package/src/components/Button.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { printName } from "typescript-package";
 
 export const Button = () => {
-  return <div>{printName("Chanwit", "Yimneam")}</div>;
+  return <div>Button</div>;
 };
