88796515ee0cd2f36bb709b261243b45a69316b6
chanwit-y <chanwit.yimneam@hotmail.com>
2022-07-13 21:09:28 +0700

    +

--- packages/react-package/src/components/Button1.tsx
+++ packages/react-package/src/components/Button1.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import {Button} from "@mui/material";
+// import { Button } from "@mui/material";
 import { printName } from "@chanwit-y/typescript-package";
 
 export const Button1 = () => {
+  let x: JSX.Element = <button>test</button>;
+  x.props = {
+    onclick: () => {
+      console.log("click hi");
+    },
+  };
+
   return (
     <div>
-      <Button>Hi beer</Button>
+      {x}
+      {/* <Button>Hi beer</Button> */}
       <div>{printName("Chanwit", "Yimneam")}</div>
     </div>
   );
